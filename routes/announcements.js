1210cea83cead0799efa2a32c798015b87e3611f
morellaj <alexjamesmorelli@gmail.com>
2020-05-14 09:15:37 -0400

    added basic announcement and calendar routes

--- routes/announcements.js
+++ routes/announcements.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 const announcements = [];
-
 const randomTime = 1589397591722;
 
 for (let i = 0; i < 10; i += 1) {
@@ -15,18 +14,9 @@ for (let i = 0; i < 10; i += 1) {
   )
 }
 
-const calendar = [];
-
-
-
-/* GET home page. */
+/* Testing out announcements with random strings */
 router.get('/announcements', function(req, res, next) {
   const reducedAnnouncements = announcements.filter(({time}) => time >= randomTime - 3 * 10000000000)
   res.send(reducedAnnouncements);
 });
-
-router.get('/calendar', function(req, res, next) {
-  res.send(calendar);
-});
-
 module.exports = router;
