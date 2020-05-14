8b14f1c07cab92ca9960f8fca4e178ed6e157987
morellaj <alexjamesmorelli@gmail.com>
2020-05-14 16:59:55 -0400

    announcements route

--- routes/announcements.js
+++ routes/announcements.js
@@ -1,22 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
-const announcements = [];
-const randomTime = 1589397591722;
-
-for (let i = 0; i < 10; i += 1) {
-  const newTime = randomTime - i * 10000000000;
-  announcements.push(
-    {
-      time: newTime,
-      text: newTime.toString().repeat(50)
-    }
-  )
-}
+const announcements = [
+  {
+    date: 'May 3',
+    event: 'Alex, Alex, and Alec became acquainted'
+  },
+  {
+    date: 'May 4',
+    event: 'Alex, Alex, and Alec confused everyone with their names'
+  },
+  {
+    date: 'May 5',
+    event: "Alex, Alex, and Alec did not care.  That was everyone else's problem."
+  }
+];
 
 /* Testing out announcements with random strings */
-router.get('/announcements', function(req, res, next) {
-  const reducedAnnouncements = announcements.filter(({time}) => time >= randomTime - 3 * 10000000000)
-  res.send(reducedAnnouncements);
+router.get('/', function(req, res, next) {
+  res.send(announcements);
 });
 module.exports = router;
