4260d402e1cab9efc31766d409e3f28bf8e9c8eb
morellaj <alexjamesmorelli@gmail.com>
2020-05-20 12:50:10 -0400

    Added connection to multiple databases

--- database.js
+++ database.js
@@ -1,6 +1,12 @@
 var pg = require('pg');
+var app = require('./app');
 
-var conString = "	postgres://jdkfzrdp:Rj2bM4jUptX89UEQEUiC0pyb6SMPZN6L@raja.db.elephantsql.com:5432/jdkfzrdp"
+var testString = 'postgres://jdkfzrdp:Rj2bM4jUptX89UEQEUiC0pyb6SMPZN6L@raja.db.elephantsql.com:5432/jdkfzrdp';
+var devString = 'postgres://xsuzfsll:sSa6qY83tfbIeykZ4Np1mROIohahR9w0@salt.db.elephantsql.com:5432/xsuzfsll';
+var prodString = 'postgres://jrzjwmlt:Y4Z6XpJfFVh4wqih79IW_CofZR1VzrbE@salt.db.elephantsql.com:5432/jrzjwmlt';
+var env = process.env.NODE_ENV;
+
+var conString = env === 'test' ? testString : env === 'production' ? prodString : devString;
 var client = new pg.Client(conString);
 
 client.connect(function(err) {
