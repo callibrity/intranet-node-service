9f77a19ed1409e72c80ee53ca81117f63fd065f6
morellaj <alexjamesmorelli@gmail.com>
2020-05-29 16:22:19 -0400

    added ability to filter employees by name

--- routes/employees.js
+++ routes/employees.js
@@ -3,6 +3,9 @@ var router = express.Router();
 var { employees } = require('../constants');
 
 router.get('/', function(req, res, next) {
+  const {findName} = req.query;
+  const arr = findName === null ? employees
+  :employees.filter(({name}) => name.toLowerCase().contains(findName.toLowerCase()))
   res.send(employees);
 });
 module.exports = router;
