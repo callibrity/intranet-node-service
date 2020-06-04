941afce62349467e6c8d4b725b0c2b8acba18496
morellaj <alexjamesmorelli@gmail.com>
2020-06-04 08:40:20 -0400

    added employee testing

--- tests/employees.test.js
+++ tests/employees.test.js
@@ -26,23 +26,8 @@ describe('Get /employees', () => {
 
 describe('Put /employees', () => {
   it('should get employees array with length 6', async () => {
-    const res = await request(app).get('/employees');
+    const res = await request(app).put('/employees?name=alec').send({name: 'Alec'});
     expect(res.statusCode).toEqual(200);
-    expect(res.body).toBeInstanceOf(Array);
-    expect(res.body.length).toBe(6);
-  })
-
-  it('should get employees array with length 1', async () => {
-    const res = await request(app).get('/employees?name=alec');
-    expect(res.statusCode).toEqual(200);
-    expect(res.body).toBeInstanceOf(Array);
-    expect(res.body.length).toBe(1);
-  })
-
-  it('should get employees array with length 0', async () => {
-    const res = await request(app).get('/employees?name=alec&office=columbus');
-    expect(res.statusCode).toEqual(200);
-    expect(res.body).toBeInstanceOf(Array);
-    expect(res.body.length).toBe(0);
+    expect(res.body).toEqual({"message": 'alec updated!'})
   })
 })
