2850494a6b0593eae7bf0f6690b84f5544dd2947
Daniel Moon <daniel_moon16@yahoo.com>
2020-06-17 10:59:23 -0400

    Fixed code after renaming database columns to lowercase

--- databases/queryStrings.js
+++ databases/queryStrings.js
@@ -1,9 +1,9 @@
 var { sqlWhereConditions, sqlUpdateValues, objectIsEmpty } = require('./helperFunctions')
 
 exports.getEmployeesQuery = ({query}) => objectIsEmpty(query) ?
-  'SELECT * FROM "Employees"' 
-  :`SELECT * FROM "Employees" WHERE ${sqlWhereConditions(query)}`
+  'SELECT * FROM employees' 
+  :`SELECT * FROM employees WHERE ${sqlWhereConditions(query)}`
 
 exports.putEmployeesQuery = ({body, query}) => (
-  `UPDATE "Employees" SET ${sqlUpdateValues(body)} WHERE ${sqlWhereConditions(query)}`
+  `UPDATE employees SET ${sqlUpdateValues(body)} WHERE ${sqlWhereConditions(query)}`
 )
--- tests/employees.test.js
+++ tests/employees.test.js
@@ -5,29 +5,28 @@ describe('Get /employees', () => {
   it('should get array', async () => {
     const res = await request(app).get('/employees') 
     expect(res.statusCode).toEqual(200)
-    expect(res.body.rows).toBeInstanceOf(Array)
-    expect(res.body.rows[0].Name).not.toBe(undefined)
+    expect(res.body).toBeInstanceOf(Array)
+    expect(res.body[0].name).not.toBe(undefined)
   })
  
   it('should get element with Name \'test\'', async () => {
     const res = await request(app).get('/employees?name=test')
     expect(res.statusCode).toEqual(200)
-    expect(res.body.rows[0].Name).toBe('test')
+    expect(res.body[0].name).toBe('test')
   })
 
   it('should get element with Name \'test\' and Office \'Cincinnati\'', async () => {
     const res = await request(app).get('/employees?name=test&office=cincinnati')
     expect(res.statusCode).toEqual(200)
-    expect(res.body.rows).toBeInstanceOf(Array)
-    expect(res.body.rows[0].Name).toBe('test')
-    expect(res.body.rows[0].Office).toBe('Cincinnati')
+    expect(res.body).toBeInstanceOf(Array)
+    expect(res.body[0].name).toBe('test')
+    expect(res.body[0].office).toBe('Cincinnati')
   })
 })
 
 describe('Put /employees', () => {
   it('should update 1 row', async () => {
-    const res = await request(app).put('/employees?name=test').send({Name: 'test'})
+    const res = await request(app).put('/employees?name=test').send({name: 'test'})
     expect(res.statusCode).toEqual(200)
-    expect(res.body.rowCount).toBe(1)
   })
 })
